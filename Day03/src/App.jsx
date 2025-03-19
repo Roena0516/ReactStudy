df54ff0ff7fb72bda2203344a2e70098441d44ab
Roena0516 <tkf2001.yc@gmail.com>
2025-03-19 10:31:10 +0900

    Map 렌더링 과제

--- Day03/src/App.jsx
+++ Day03/src/App.jsx
@@ -1,6 +1,8 @@
 import PackingList2 from "./Condition2";
 import MapTest1 from "./MapTest1";
 import MapTest2 from "./MapTest2";
+import MapTest3 from "./MapTest3";
+import MapTest4 from "./MapTest4";
 import PackingList from "./PackingList";
 
 export default function App() {
@@ -12,6 +14,8 @@ export default function App() {
       <MapTest2 />
       <PackingList />
       <PackingList2 />
+      <MapTest3 />
+      <MapTest4 />
     </div>
   )
 }
