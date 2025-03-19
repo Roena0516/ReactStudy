6cb4b214f1e97057a9e3a7676555f9fbe63d414b
Roena0516 <tkf2001.yc@gmail.com>
2025-03-19 09:13:06 +0900

    add MapTest1.jsx

--- Day03/src/App.jsx
+++ Day03/src/App.jsx
@@ -1,9 +1,11 @@
+import MapTest1 from "./MapTest1";
+
 export default function App() {
 
 
   return (
     <div>
-      
+      <MapTest1 />
     </div>
   )
 }
--- Day03/src/MapTest1.jsx
+++ Day03/src/MapTest1.jsx
@@ -13,13 +13,17 @@ function MapTest1() {
     <>
       <h1>foo 배열 출력하기</h1>
       <ul>
-        {/* {작성하세요.} */}
+        {foo.map((item) => {
+            return <li>{item}</li>
+        })}
         
       </ul>
       <h1>차고에 있는 차 목록</h1>
       <ul>
         {/*cars 배열 요소를 Car컴포넌트의 brand로 전송하여 컴포넌트 리턴 */}
-        {/* { 작성하세요.} */}
+        {cars.map((item) => {
+            return <Car brand = {item}/>
+        })}
       </ul>
     </>
   );
