8b6072b5fcf9cbc97e1b121fa0b301463655eed1
Roena0516 <tkf2001.yc@gmail.com>
2025-04-09 10:21:23 +0900

    useNavigate 실습

--- Day06/src/pages/about.jsx
+++ Day06/src/pages/about.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const About = () => {
+    const navigate = useNavigate();
     return (
-        <h1>소개페이지입니다.</h1>
+        <div>
+            <h1>소개페이지입니다.</h1>
+            <button onClick={() => navigate("/")}>홈으로</button>
+            <button onClick={() => navigate(-1)}>뒤로가기</button>
+        </div>
     )
 }
 
